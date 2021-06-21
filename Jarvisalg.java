1d016921889309a19099d3153bf2ee78d14e88b5
stasadragovic <78707825+stasadragovic@users.noreply.github.com>
2021-06-21 17:33:34 +0200

    Add files via upload
    ispisi metoda

--- Jarvisalg.java
+++ Jarvisalg.java
@@ -17,7 +17,14 @@ public class Jarvisalg {
 
     public static class Jarvis {
 
-        
+        public void ispisi(Point[] points, int[] next) {
+            System.out.println("\ntacke : ");
+
+            for (int i = 0; i < next.length; i++)
+                if (next[i] != -1) {
+                    System.out.println("( " + points[i].x + ", " + points[i].y + " )");
+                }
+        }
 
         
 
